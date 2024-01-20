6f45854e80bd71fc9c0ca92038e3fdde56d09d22
Jonathan Ricardo <jonathan_levelup@outlook.com>
2024-01-20 01:22:33 -0300

    Using DTO pattern to return users on UserResource

--- src/main/java/com/binaryz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/binaryz/workshopmongo/resources/UserResource.java
@@ -1,12 +1,14 @@
 package com.binaryz.workshopmongo.resources;
 
 import com.binaryz.workshopmongo.domain.User;
+import com.binaryz.workshopmongo.dto.UserDTO;
 import com.binaryz.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -17,9 +19,12 @@ public class UserResource {
 
     //@RequestMapping(method= RequestMethod.GET)
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){ // Capsule the list of user as an http entity
-        List<User> list = userService.findAll(); // Instantiate a list and get all users using the instantiated service method.
-        return ResponseEntity.ok().body(list); // return http and list as body.
+    public ResponseEntity<List<UserDTO>> findAll(){ // Capsule the list of user as an http entity
+        List<User> listUsers = userService.findAll(); // Instantiate a list and get all users using the instantiated service method.
+        List<UserDTO> listDTOUsers = listUsers.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); /*Get the listUsers to a stream
+         then get each User object from the list and convert into an UserDTO object, so collect all this new objects and use all them to
+         form a new UserDTO list*/
+        return ResponseEntity.ok().body(listDTOUsers); // return http and list as body.
     }
 
 
