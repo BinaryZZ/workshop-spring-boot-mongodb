9944a8774abefdb6a56b9a9c04e4dec9b6d41f95
Jonathan Ricardo <jonathan_levelup@outlook.com>
2024-01-23 16:54:34 -0300

    Projecting author data with DTO

--- src/main/java/com/binaryz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/binaryz/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.binaryz.workshopmongo.config;
 
 import com.binaryz.workshopmongo.domain.Post;
 import com.binaryz.workshopmongo.domain.User;
+import com.binaryz.workshopmongo.dto.AuthorDTO;
 import com.binaryz.workshopmongo.repository.PostRepository;
 import com.binaryz.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,11 +36,11 @@ public class Instantiation implements CommandLineRunner { // Implementing a inte
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra São Paulo, Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra São Paulo, Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
--- src/main/java/com/binaryz/workshopmongo/domain/Post.java
+++ src/main/java/com/binaryz/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.binaryz.workshopmongo.domain;
 
+import com.binaryz.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,12 +19,12 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post(){
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
