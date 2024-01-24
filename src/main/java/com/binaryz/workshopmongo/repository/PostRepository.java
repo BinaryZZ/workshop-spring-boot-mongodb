21b4df5f1cc58b6f6f558edb6420ed64581c998d
Jonathan Ricardo <jonathan_levelup@outlook.com>
2024-01-24 00:28:56 -0300

    Simple consult with @Query

--- src/main/java/com/binaryz/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/binaryz/workshopmongo/repository/PostRepository.java
@@ -2,12 +2,16 @@ package com.binaryz.workshopmongo.repository;
 
 import com.binaryz.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> { // MongoRepository<Class domain type, ID type>
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // { 'attribute to be searched': { $regex: regularExpression(0 to the first object = text), $options: 'i to ignore sensitive case' } }
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
